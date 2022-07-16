41e8774e6f9163b04d52d40e0b475e8667885ef1
Rohan Dhawale <79928470+ronny-2001@users.noreply.github.com>
2022-07-16 22:13:17 +0530

    Add files via upload

---
+++ Game/Main.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		guessTheWord game=new guessTheWord();
+		
+		game.start();
+		game.end();
+	}
+
+}
